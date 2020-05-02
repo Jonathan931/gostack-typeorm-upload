e71428262480bdd1b047a4c4296d954b9906be45
Jonathan Matheus <jonathanmatheus@MacBook-de-Jonathan.local>
2020-05-02 16:03:31 -0300

    Commit desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,33 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        const newAccumulator = accumulator;
+        switch (transaction.type) {
+          case 'income':
+            newAccumulator.income += Number(transaction.value);
+            break;
+          case 'outcome':
+            newAccumulator.outcome += Number(transaction.value);
+            break;
+          default:
+            break;
+        }
+        return newAccumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
